deaecc55a37651dc501446898be52ac532ac3113
Paradoxollie <129663056+Paradoxollie@users.noreply.github.com>
2023-04-24 11:50:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,85 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Codenames Game</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-image: linear-gradient(135deg, #7f7fd5, #86a8e7, #91eae4);
-      background-size: 400% 400%;
-      animation: gradientBackground 10s ease infinite;
-      text-align: center;
+const words = [  "AFRICA", "AGENT", "AIR", "ALIEN", "ALPS", "AMAZON", "AMBULANCE", "AMERICA", "ANGEL", "ANTARCTICA", "APPLE", "ARM", "ATLANTIS", "AUSTRALIA", "AZTEC", "BACK", "BALL", "BAND", "BANK", "BAR", "BARK", "BAT", "BATTERY", "BEACH", "BEAR", "BEAT", "BED", "BEIJING", "BELL", "BELT", "BERLIN", "BERMUDA", "BERRY", "BILL", "BLOCK", "BOARD", "BOLT", "BOMB", "BOND", "BOOM", "BOOT", "BOTTLE", "BOW", "BOX", "BRIDGE", "BRUSH", "BUCK", "BUFFALO", "BUG", "BUGLE", "BUTTON", "CALF", "CANADA", "CAP", "CAPITAL", "CAR", "CARD", "CARROT", "CASINO", "CAST", "CAT", "CELL", "CENTAUR", "CENTER", "CHAIR", "CHANGE", "CHARGE", "CHECK", "CHEST", "CHICK", "CHINA", "CHOCOLATE", "CHURCH", "CIRCLE", "CLIFF", "CLOAK", "CLUB", "CODE", "COLD", "COMIC","COMPOUND", "CONCERT", "CONDUCTOR", "CONTRACT", "COOK", "COPPER", "COTTON", "COURT", "COVER", "CRANE", "CRASH", "CRICKET", "CROSS", "CROWN", "CYCLE", "CZECH", "DANCE", "DATE", "DAY", "DEATH", "DECK", "DEGREE", "DIAMOND", "DICE", "DINOSAUR", "DISEASE", "DOCTOR", "DOG", "DRAFT", "DRAGON", "DRESS", "DRILL", "DROP", "DUCK", "DWARF", "EAGLE", "EGYPT", "EMBASSY", "ENGINE", "ENGLAND", "EUROPE", "EYE", "FACE", "FAIR", "FALL", "FAN", "FENCE", "FIELD", "FIGHTER", "FIGURE", "FILE", "FILM", "FIRE", "FISH", "FLUTE", "FLY", "FOOT", "FORCE", "FOREST", "FORK", "FRANCE", "GAME", "GAS", "GENIUS", "GERMANY", "GHOST", "GIANT", "GLASS", "GLOVE", "GOLD", "GRACE", "GRASS", "GREECE", "GREEN", "GROUND", "HAM", "HAND", "HAWK", "HEAD", "HEART", "HELICOPTER", "HIMALAYAS", "HOLE", "HOLLYWOOD", "HONEY", "HOOD", "HOOK", "HORN", "HORSE", "HORSESHOE", "HOSPITAL", "HOTEL", "ICE", "ICE CREAM", "INDIA", "IRON", "IVORY", "JACK", "JAM", "JET", "JUPITER", "KANGAROO", "KETCHUP", "KEY", "KID", "KING", "KIWI", "KNIFE", "KNIGHT", "LAB", "LAP", "LASER", "LAWYER", "LEAD", "LEMON", "LEPRECHAUN", "LIFE", "LIGHT", "LIMOUSINE", "LINE", "LINK", "LION", "LITTER", "LOCH NESS", "LOCK", "LOG", "LONDON", "LUCK", "MAIL", "MAMMOTH", "MAPLE", "MARBLE", "MARCH", "MASS", "MATCH", "MERCURY", "MEXICO", "MICROSCOPE", "MILLIONAIRE", "MINE", "MINT", "MISSILE", "MODEL", "MOLE", "MOON", "MOSCOW", "MOUNT", "MOUSE", "MOUTH", "MUG", "NAIL", "NEEDLE", "NET", "NEW YORK", "NIGHT", "NINJA", "NOTE", "NOVEL", "NURSE", "NUT","OCTOPUS", "OIL", "OLIVE", "OLYMPUS", "OPERA", "ORANGE", "ORGAN", "PALM", "PAN", "PANTS", "PAPER", "PARACHUTE", "PARK", "PART", "PASS", "PASTE", "PENGUIN", "PHOENIX", "PIANO", "PIE", "PILOT", "PIN", "PIPE", "PIRATE", "PISTOL", "PIT", "PITCH", "PLANE", "PLASTIC", "PLATE", "PLATYPUS", "PLAY", "PLOT", "POINT", "POISON", "POLE", "POLICE", "POOL", "PORT", "POST", "POUND", "PRESS", "PRINCESS", "PUMPKIN", "PUPIL", "PYRAMID", "QUEEN", "RABBIT", "RACKET", "RAY", "REVOLUTION", "RING", "ROBIN", "ROBOT", "ROCK", "ROME", "ROOT", "ROSE", "ROULETTE", "ROUND", "ROW", "RULER", "SATELLITE", "SATURN", "SCALE", "SCHOOL", "SCIENTIST", "SCORPION", "SCREEN", "SCUBA DIVER", "SEAL", "SERVER", "SHADOW", "SHAKESPEARE", "SHARK", "SHIP", "SHOE", "SHOP", "SHOT", "SINK", "SKYSCRAPER", "SLIP", "SLUG", "SMUGGLER", "SNOW", "SNOWMAN", "SOCK", "SOLDIER", "SOUL", "SOUND", "SPACE", "SPELL", "SPIDER", "SPIKE", "SPINE", "SPOT", "SPRING", "SPY", "SQUARE", "STADIUM", "STAFF", "STAR", "STATE", "STICK", "STOCK", "STRAW", "STREAM", "STRIKE", "STRING", "SUB", "SUIT", "SUPERHERO", "SWING", "SWITCH", "TABLE", "TABLET", "TAG", "TAIL", "TAP", "TEACHER", "TELESCOPE", "TEMPLE", "THEATER", "THIEF", "THUMB", "TICK", "TIE", "TIME", "TOKYO", "TOOTH", "TORCH", "TOWER", "TRACK", "TRAIN", "TRIANGLE", "TRIP", "TRUNK", "TUBE", "TURKEY", "UNDERTAKER", "UNICORN", "VACUUM", "VAN", "VET", "WAKE", "WALL", "WAR", "WASHER", "WASHINGTON", "WATCH", "WATER", "WAVE", "WEB", "WELL", "WHALE", "WHIP", "WIND", "WITCH", "WORM", "YARD" 
+];
+<div class="game-board"></div>
+<button id="toggleSpyMode">Toggle Spy Mode</button>
+<script>
+const gameBoard = document.querySelector('.game-board');
+const toggleSpyModeButton = document.getElementById('toggleSpyMode');
+const numCards = 25;
+const numBlueCards = 9;
+const numRedCards = 9;
+const numWhiteCards = 4;
+const numBlackCards = 3;
+
+function generateRandomIndices(limit, count) {
+  const indices = [];
+  while (indices.length < count) {
+    const index = Math.floor(Math.random() * limit);
+    if (!indices.includes(index)) {
+      indices.push(index);
     }
+  }
+  return indices;
+}
 
-    @keyframes gradientBackground {
-      0% { background-position: 0% 50%; }
-      50% { background-position: 100% 50%; }
-      100% { background-position: 0% 50%; }
+function generateCards() {
+  const cardIndices = generateRandomIndices(words.length, numCards);
+  const blueIndices = generateRandomIndices(numCards, numBlueCards);
+  const redIndices = generateRandomIndices(numCards, numRedCards);
+  const blackIndices = generateRandomIndices(numCards, numBlackCards);
+
+  const cards = cardIndices.map((wordIndex, i) => {
+    const word = words[wordIndex];
+    let color = 'white';
+    if (blueIndices.includes(i)) {
+      color = 'blue';
+    } else if (redIndices.includes(i)) {
+      color = 'red';
+    } else if (blackIndices.includes(i)) {
+      color = 'black';
     }
+    return { word, color };
+  });
 
-    .board {
-      display: grid;
-      grid-template-columns: repeat(5, 1fr);
-      grid-gap: 10px;
-      width: 80%;
-      margin: 0 auto;
-      margin-top: 50px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-      border-radius: 10px;
-      padding: 20px;
-      background-color: #ffffff;
+  return cards;
+}
+
+function renderGameBoard(cards, showColors = false) {
+  gameBoard.innerHTML = '';
+  cards.forEach((card, i) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+    cardElement.textContent = card.word;
+    cardElement.addEventListener('click', () => {
+      if (!cardElement.classList.contains('revealed')) {
+        cardElement.classList.add('revealed');
+        cardElement.classList.add(card.color);
+      }
+    });
+    
+    if (showColors) {
+      cardElement.classList.add('revealed');
+      cardElement.classList.add(card.color);
     }
+    
+    gameBoard.appendChild(cardElement);
+  });
+}
 
-    .card {
-      background-color: #f1f1f1;
-      padding: 20px;
-      text-align: center;
-      cursor: pointer;
-      user-select: none;
-      height: 80px;
-      border-radius: 5px;
-      box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-      transition: all 0.3s ease;
-    }
+let cards = generateCards();
+let spyModeEnabled = false;
 
-    .card:hover {
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-      transform: translateY(-5px);
-    }
+renderGameBoard(cards);
 
-    .card.revealed {
-      box-shadow: none;
-      transform: translateY(0);
-    }
-
-    .red {
-      background-color: #f1f1f1;
-      color: red;
-    }
-
-    .blue {
-      background-color: #f1f1f1;
-      color: blue;
-    }
-
-    .neutral {
-      background-color: #f1f1f1;
-      color: gray;
-    }
-
-    .black {
-      background-color: #f1f1f1;
-      color: black;
-    }
-  </style>
-</head>
-<body>
-  <div class="board"></div>
-
-  <button id="toggleSpyMode">Toggle Spy Mode</button>
-
-  <script src="script.js"></script>
+toggleSpyModeButton.addEventListener('click', () => {
+  spyModeEnabled = !spyModeEnabled;
+  renderGameBoard(cards, spyModeEnabled);
+});
+</script>
 </body>
 </html>
